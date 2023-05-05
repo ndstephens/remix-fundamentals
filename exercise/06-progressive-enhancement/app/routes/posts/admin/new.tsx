6e42755976d1d65dda4c82de25f0d91dd6befd6e
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-05-04 21:18:55 -0400

    loading states and prefetching

--- exercise/06-progressive-enhancement/app/routes/posts/admin/new.tsx
+++ exercise/06-progressive-enhancement/app/routes/posts/admin/new.tsx
@@ -1,11 +1,14 @@
 import type { ActionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
 import { createPost } from "~/models/post.server";
 
 export async function action({ request }: ActionArgs) {
+  // create a one second delay to simulate a slow network
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   const formData = await request.formData();
 
   const title = formData.get("title");
@@ -39,6 +42,8 @@ export default function NewPost() {
   // 🐨 get the transition from useTransition
   // 🦉 make sure that useTransition comes from "@remix-run/react" and NOT "react"!!
   // 🐨 determine whether we're creating by whether there's a transition.submission object.
+  const transition = useTransition();
+  const isSubmitting = Boolean(transition.submission);
 
   return (
     <Form method="post">
@@ -80,9 +85,10 @@ export default function NewPost() {
           type="submit"
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
           // 🐨 add a disabled attribute to the button if we're creating
+          disabled={isSubmitting}
         >
           {/* 🐨 Have this say "Creating..." if we're in a creating state */}
-          Create Post
+          {isSubmitting ? "Creating..." : "Create Post"}
         </button>
       </p>
     </Form>
